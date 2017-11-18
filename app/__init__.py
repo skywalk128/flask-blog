4d7021d07e651ea9b6f104a01a1e9d4f5621e5a3
skywalk128 <cgx_2010@163.com>
2017-11-18 09:49:53 +0800

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+import datetime
 
 app = Flask(__name__)
 app.config.from_object("config")
+app.config['SECRET_KEY'] = str(datetime.datetime)
+
+db = MongoEngine(app)
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,6 +1,46 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from .models import Todo, TodoForm
 
 @app.route('/')
 def index():
-    return render_template("index.html", text="Hello World")
+    form = TodoForm()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos, form=form)#返回模板和所有的todo
+
+@app.route('/add', methods=['POST', ])
+def add():
+    form = TodoForm(request.form)
+    if form.validate():
+        content = form.content.data
+        todo = Todo(content=content)
+        todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos, form=form)
+
+@app.route('/done/<string:todo_id>')
+def done(todo_id):
+    form = TodoForm()
+    todo = Todo.objects.get_or_404(id=todo_id)
+    todo.status = 1
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos, form=form)
+
+
+@app.route('/undone/<string:todo_id>')
+def undone(todo_id):
+    form = TodoForm()
+    todo = Todo.objects.get_or_404(id=todo_id)
+    todo.status = 0
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos, form=form)
+
+@app.route('/delete/<string:todo_id>')
+def delete(todo_id):
+    form = TodoForm()
+    todo = Todo.objects.get_or_404(id=todo_id)
+    todo.delete()
+    todos = Todo.objects.all()
+    return render_template('index.html', todos=todos, form=form)
